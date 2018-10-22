50a005a6430ea04406377528fc2e9d99d68cbdd8
cwijayasundara <cwijayasundara@gmail.com>
2018-10-22 14:08:38 +0100

    Added support for message selectors

--- src/main/java/com/cham/webspheremqjms/WebsphereMqJmsApplication.java
+++ src/main/java/com/cham/webspheremqjms/WebsphereMqJmsApplication.java
@@ -18,14 +18,20 @@ import org.springframework.jms.support.converter.MessageType;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 @EnableJms
 public class WebsphereMqJmsApplication {
 
 	@Bean
-	public JmsListenerContainerFactory<?> JmsListenerContainerFac(ConnectionFactory connectionFactory,
-																  DefaultJmsListenerContainerFactoryConfigurer configurer) {
+	public JmsListenerContainerFactory<?> JmsListenerContainerFactory(ConnectionFactory connectionFactory,
+																	  DefaultJmsListenerContainerFactoryConfigurer configurer) {
 		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
 		configurer.configure(factory, connectionFactory);
 		return factory;
@@ -52,5 +58,34 @@ public class WebsphereMqJmsApplication {
 			}
 		});
 		jmsTemplate.convertAndSend("DEV.QUEUE.1", new Tweet("Bar", "I like Cake"));
+
+		dateConversion();
+	}
+
+	private static void dateConversion(){
+		String originalDate = "2018-10-29 00:55";
+		LocalDateTime localTime = LocalDateTime.parse(originalDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+		System.out.println("The local time is " + localTime);
+
+		ZonedDateTime utcTime = localTime.atZone(ZoneOffset.UTC);
+		ZonedDateTime bstTime = utcTime.withZoneSameInstant(ZoneId.of("Europe/London"));
+
+		System.out.println("The BST time is " + bstTime);
+
+		// the summer time offset is how many milliseconds?
+		long dstOffset = ChronoUnit.MILLIS.between(utcTime.toLocalDateTime(), bstTime.toLocalDateTime());
+		System.out.println(dstOffset); // prints 0
+
+      // try the same at start of day (midnight)
+		utcTime = utcTime.toLocalDate().atStartOfDay(ZoneOffset.UTC);
+		bstTime = utcTime.withZoneSameInstant(ZoneId.of("Europe/London"));
+		dstOffset = ChronoUnit.MILLIS.between(utcTime.toLocalDateTime(), bstTime.toLocalDateTime());
+		System.out.println(dstOffset); // prints 3600000
+
+      // and next midnight
+		utcTime = utcTime.plusDays(1);
+		bstTime = utcTime.withZoneSameInstant(ZoneId.of("Europe/London"));
+		dstOffset = ChronoUnit.MILLIS.between(utcTime.toLocalDateTime(), bstTime.toLocalDateTime());
+		System.out.println(dstOffset);
 	}
 }
--- src/main/java/com/cham/webspheremqjms/consumer/WebsphereMqListener.java
+++ src/main/java/com/cham/webspheremqjms/consumer/WebsphereMqListener.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class WebsphereMqListener {
 
-     @JmsListener(destination = "DEV.QUEUE.1", containerFactory = "JmsListenerContainerFac", selector = "tweet_prop='exec'")
-     //@JmsListener(destination = "DEV.QUEUE.1", containerFactory = "JmsListenerContainerFac")
+     @JmsListener(destination = "DEV.QUEUE.1", containerFactory = "JmsListenerContainerFactory", selector = "tweet_prop='exec'")
+     //@JmsListener(destination = "DEV.QUEUE.1", containerFactory = "JmsListenerContainerFactory")
     public void receiveMessage(Tweet tweet) {
         System.out.println("Received <" + tweet + ">");
     }
